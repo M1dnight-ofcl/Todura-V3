157721d0dc124fc68b4b12b05f00c57ebc0ee98b
M1dnight <108243683+M1dnight-ofcl@users.noreply.github.com>
2025-02-05 18:20:51 -0500

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Todura V3
+Written with Wails
+n werd
---
+++ frontend/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'; 
+import { Greet } from "../wailsjs/go/main/App";
+import { Tabs } from '@base-ui-components/react/tabs';
+import { motion } from "motion/react";
+import "./styles.scss";
+import "./home.scss";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGear, faHome } from '@fortawesome/free-solid-svg-icons';
+const App=()=>{
+  return (<>
+    <motion.div id="AppWrapper" className="default_dark">
+      <Tabs.Root className="Tabs" defaultValue="home">
+        <Tabs.List className="List">
+          <Tabs.Tab className="Tab" value="home"><FontAwesomeIcon icon={faHome} /></Tabs.Tab>
+          <Tabs.Tab className="Tab" value="settings"><FontAwesomeIcon icon={faGear} /></Tabs.Tab>
+          <Tabs.Indicator className="Indicator" />
+        </Tabs.List>
+        <Tabs.Panel className="Panel" value="home">
+
+        </Tabs.Panel>
+        <Tabs.Panel className="Panel" value="settings">
+          
+        </Tabs.Panel>
+      </Tabs.Root>
+    </motion.div>
+  </>);
+}
+export default App;
