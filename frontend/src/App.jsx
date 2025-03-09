ad387f928782b957f86f31dbb5a4c1aba9dddabb
M1dnight <108243683+M1dnight-ofcl@users.noreply.github.com>
2025-03-09 01:08:41 -0500

    added search concept + add menu (disfunctional idk if this was in the last commit or not but who gaf) yayay

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,51 @@
-import { useState } from 'react'; 
+import { useEffect, useState } from 'react'; 
 import { Greet } from "../wailsjs/go/main/App";
-import { Tabs } from '@base-ui-components/react/tabs';
 import { motion } from "motion/react";
 import "./styles.scss";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGear, faHome } from '@fortawesome/free-solid-svg-icons';
+import { faClose, faCompress, faExpand, faGear, faHome, faMaximize, faMinimize, faMinus, faMinusSquare, faSearch } from '@fortawesome/free-solid-svg-icons';
 import { Home } from './modules/Home';
+import { Input, Tabs, Dialog } from '@base-ui-components/react';
 const App=()=>{
-  return (<>
-    <motion.div id="AppWrapper" className="default_dark">
+  const[maximized,setMaximized]=useState(false);
+  useEffect(()=>{
+    new ResizeObserver(()=>{
+      window.runtime.WindowIsMaximised().then(val=>setMaximized(val));
+    }).observe(document.getElementById("AppWrapper"));
+  },[]);
+  const TitleBar=({})=>{
+    return(<div id="Titlebar" className="default_dark" data-wails-drag>
+      <motion.div id="TBIcon"></motion.div>
+      <motion.div id="ActionButtonWrapper">
+        {/* window.runtime.WindowMinimise() */}
+        <motion.button id="Close" className='ActionButton'
+          onClick={()=>{window.runtime.Quit();}}>
+            <FontAwesomeIcon icon={faClose}/></motion.button>
+        <motion.button id="Maximize" className='ActionButton'
+          onClick={()=>{
+            window.runtime.WindowToggleMaximise();
+            setMaximized(!maximized);
+          }}><FontAwesomeIcon icon={maximized?faCompress:faExpand}/></motion.button>
+        <motion.button id="Minimize" className='ActionButton'
+          onClick={()=>{window.runtime.WindowMinimise();}}>
+            <FontAwesomeIcon icon={faMinus}/></motion.button>
+      </motion.div>
+      <Dialog.Root>
+        <Dialog.Trigger className="SearchTrigger">
+          <motion.div id="Search" children="Search" />
+        </Dialog.Trigger>
+        <Dialog.Portal keepMounted>
+          <Dialog.Backdrop className="SearchBackdrop" />
+          <Dialog.Popup className="SearchPopup">
+            <h1>Hello World</h1>
+          </Dialog.Popup>
+        </Dialog.Portal>
+      </Dialog.Root>
+    </div>);
+  }
+  return(<>
+    <TitleBar/>
+    <motion.div id="AppWrapper" className="default_dark" data-wails-no-drag>
       <Tabs.Root className="Tabs" defaultValue="home">
         <Tabs.List className="List">
           <Tabs.Tab className="Tab" value="home"><FontAwesomeIcon icon={faHome} /></Tabs.Tab>
