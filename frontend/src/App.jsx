9d34f3e6d3f1545934dde054903d8ec524e748cf
M1dnight <108243683+M1dnight-ofcl@users.noreply.github.com>
2025-03-09 19:06:02 -0400

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faClose, faCompress, faExpand, faGear, faHome, faMaximize, faMinimize, faMinus, faMinusSquare, faSearch } from '@fortawesome/free-solid-svg-icons';
 import { Home } from './modules/Home';
 import { Input, Tabs, Dialog } from '@base-ui-components/react';
+import { categories as _categories, tasks as _tasks } from './store/store';
+import { useAtom } from 'jotai';
 const App=()=>{
   const[maximized,setMaximized]=useState(false);
   useEffect(()=>{
@@ -14,7 +16,10 @@ const App=()=>{
     }).observe(document.getElementById("AppWrapper"));
   },[]);
   const TitleBar=({})=>{
-    return(<div id="Titlebar" className="default_dark" data-wails-drag>
+    const[categories,_1]=useAtom(_categories);
+    const[tasks,_2]=useAtom(_tasks);
+    const[SearchRes,setSearchRes]=useState([...tasks]);
+    return(<div id="Titlebar"  data-wails-drag>
       <motion.div id="TBIcon"></motion.div>
       <motion.div id="ActionButtonWrapper">
         {/* window.runtime.WindowMinimise() */}
@@ -32,12 +37,29 @@ const App=()=>{
       </motion.div>
       <Dialog.Root>
         <Dialog.Trigger className="SearchTrigger">
-          <motion.div id="Search" children="Search" />
+          <motion.div className="Search" children="Search" onClick={(e)=>{
+            document.getElementById("SearchPopup").style.width=
+              `${e.target.getBoundingClientRect().width}px`;
+          }} />
         </Dialog.Trigger>
         <Dialog.Portal keepMounted>
           <Dialog.Backdrop className="SearchBackdrop" />
-          <Dialog.Popup className="SearchPopup">
-            <h1>Hello World</h1>
+          <Dialog.Popup id="SearchPopup" >
+            <Input placeholder='Search' id="Search" onChange={(e)=>{
+
+            }} />
+            <motion.div className='SearchGroup' id="SearchProperties">
+              {categories.map((data)=>
+                <motion.div key={data.id} className='SearchResult'>
+                  <span className='SRType Category'>Category</span><span className='SRScope Category'>{data.title}</span>
+                </motion.div>)}
+            </motion.div>
+            <motion.div className='SearchGroup' id="SearchResults">
+              {SearchRes.map((data)=>
+                <motion.div key={data.id} className='SearchResult'>
+                  {data.title}
+                </motion.div>)}
+            </motion.div>
           </Dialog.Popup>
         </Dialog.Portal>
       </Dialog.Root>
@@ -45,7 +67,7 @@ const App=()=>{
   }
   return(<>
     <TitleBar/>
-    <motion.div id="AppWrapper" className="default_dark" data-wails-no-drag>
+    <motion.div id="AppWrapper" data-wails-no-drag>
       <Tabs.Root className="Tabs" defaultValue="home">
         <Tabs.List className="List">
           <Tabs.Tab className="Tab" value="home"><FontAwesomeIcon icon={faHome} /></Tabs.Tab>
