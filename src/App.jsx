8d3ce9d5c6982694e196d7d6a32f9566e44b63fa
M1dnight <108243683+M1dnight-ofcl@users.noreply.github.com>
2025-04-06 01:51:51 -0400

    Started work on menu and changed icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { useEffect, useState } from 'react'; 
-import { motion } from "motion/react";
+import { useEffect, useState, useSyncExternalStore } from 'react'; 
+import { AnimatePresence, motion } from "motion/react";
 import "./styles.scss";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faClose, faCompress, faExpand, faGear, faHome, faMaximize, faMinimize, faMinus, faMinusSquare, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faClose, faCompress, faCopyright, faExpand, faFile, faGear, faHeart, faHome, faInfo, faMaximize, faMinimize, faMinus, faMinusSquare, faSearch } from '@fortawesome/free-solid-svg-icons';
 import { Home } from './modules/Home';
 import { Input, Tabs, Dialog } from '@base-ui-components/react';
 import { categories as _categories, tasks as _tasks } from './store/store';
@@ -10,26 +10,112 @@ import { useAtom } from 'jotai';
 // import { appWindow } from '@tauri-apps/api/window';
 import { getCurrentWindow } from '@tauri-apps/api/window';
 import { isTauri } from '@tauri-apps/api/core';
+import { createPortal } from 'react-dom';
 const App=()=>{
   const[maximized,setMaximized]=useState(false);
-  const windowMaxListener=getCurrentWindow().onResized(({payload:size})=>{
+  /* let windowMaxListener=getCurrentWindow().onResized(({payload:size})=>{
     getCurrentWindow().isMaximized().then(res=>setMaximized(res));
     console.log("Resized Window", size, maximized);
-  });
+  }); */
   useEffect(()=>{
-    /* new ResizeObserver(()=>{
-      // window.runtime.WindowIsMaximised().then(val=>setMaximized(val));
-      setMaximized(getCurrentWindow().isMaximized());
-    }).observe(document.getElementById("AppWrapper")); */
-    windowMaxListener();
+    new ResizeObserver(()=>{
+      getCurrentWindow().isMaximized().then(val=>{
+        // console.log(val);
+        setMaximized(val);
+      });
+    }).observe(document.getElementById("AppWrapper"));
+    // windowMaxListener();
   },[]);
   const TitleBar=({})=>{
     const[categories,_1]=useAtom(_categories);
     const[tasks,_2]=useAtom(_tasks);
     const[SearchRes,setSearchRes]=useState([...tasks]);
     const[SearchPrompt,setSearchPrompt]=useState("");
-    return(<div id="Titlebar"  data-tauri-drag-region /* data-wails-drag */>
-      <motion.div id="TBIcon"></motion.div>
+    const[Menu,setMenu]=useState(false);
+    return(<div id="Titlebar" data-tauri-drag-region /* data-wails-drag */>
+      {createPortal(<>
+        <motion.div 
+          id="TBIcon"
+          className={Menu?"MenuOpen":""}
+          onClick={(e)=>{
+            e.preventDefault();
+            setMenu(!Menu);
+          }}></motion.div>
+        <AnimatePresence>
+          {Menu?<> 
+            <motion.div 
+              className="Menu"
+              transition={{duration:.35}}
+              initial={{opacity:0,}}
+              exit={{opacity:0,}}
+              whileInView={{opacity:1,}}>
+                <motion.button
+                  className="CloseButton"
+                  transition={{duration:.35}}
+                  initial={{opacity:0,}}
+                  exit={{opacity:0,}}
+                  whileInView={{opacity:1,}}
+                  onClick={(e)=>{
+                    e.preventDefault();
+                    setMenu(false);
+                  }}>
+                    <FontAwesomeIcon icon={faClose} />
+                </motion.button>
+                <motion.div
+                  className="MenuWrapper"
+                  transition={{duration:.25,delay:.15}}
+                  initial={{opacity:0,x:-35}}
+                  exit={{opacity:0,x:-35}}
+                  whileInView={{opacity:1,x:0}}>
+                    <Tabs.Root className="MenuTabs" defaultValue="info">
+                      <Tabs.List className="MenuList">
+                        <Tabs.Tab className="MenuTab" value="info"><FontAwesomeIcon icon={faInfo} />&nbsp;&nbsp;Info</Tabs.Tab>
+                        <Tabs.Tab className="MenuTab" value="file"><FontAwesomeIcon icon={faFile} />&nbsp;&nbsp;File</Tabs.Tab>
+                        <Tabs.Indicator className="MenuIndicator" />
+                      </Tabs.List>
+                      <Tabs.Panel className="MenuPanel" value="info">
+                        <motion.p
+                          transition={{duration:.25,delay:.25}}
+                          initial={{opacity:0,x:-15}}
+                          exit={{opacity:0,x:-15}}
+                          whileInView={{opacity:1,x:0}}>
+                            Version x.xx.xx
+                        </motion.p>
+                        <motion.p
+                          transition={{duration:.25,delay:.35}}
+                          initial={{opacity:0,x:-15}}
+                          exit={{opacity:0,x:-15}}
+                          whileInView={{opacity:1,x:0}}>
+                            M1dnight <FontAwesomeIcon icon={faCopyright} /> 2025
+                        </motion.p>
+                        <motion.p
+                          transition={{duration:.35,delay:.35}}
+                          initial={{opacity:0,x:-15}}
+                          exit={{opacity:0,x:-15}}
+                          whileInView={{opacity:1,x:0}}>
+                            Made with <FontAwesomeIcon icon={faHeart} /> in NJ
+                        </motion.p>
+                      </Tabs.Panel>
+                      <Tabs.Panel className="MenuPanel" value="file">
+                        <motion.button
+                          transition={{duration:.25}}
+                          initial={{opacity:0,x:-15}}
+                          exit={{opacity:0,x:-15}}
+                          whileInView={{opacity:1,x:0}}
+                          onClick={(e)=>{
+                            e.preventDefault();
+                            if(isTauri())getCurrentWindow().close();
+                            else close();
+                          }}>
+                            Exit
+                        </motion.button>
+                      </Tabs.Panel>
+                    </Tabs.Root>
+                </motion.div>
+            </motion.div>
+          </>:null}
+        </AnimatePresence>
+      </>,document.body)}
       <motion.div id="ActionButtonWrapper">
         {isTauri()?<>
           <motion.button id="Close" className='ActionButton'
